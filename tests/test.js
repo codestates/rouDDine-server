cbb53fdb89b4acc0118fbc26eb5c395e6c3d690e
leegeonwoo <kwoo.chris@gmail.com>
2022-09-07 06:25:02 +0900

    #9 supertest 설치

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,29 @@
-test("1 is 1", () => {
-    expect(1).toBe(1);
-  });
+const app = require('../src/app')
+const https = require('https') 
+// 
+const request = require('supertest');
+const agent = requrest(app)
+const users = require('../src/controller/signcontroller/users');
+
+// 1. 서버접속 테스트
+
+// 2. 회원 가입 테스트
+
+// 3. 로그인 테스트
+
+// 4. 로그아웃 테스트
+
+// 5. 임시로그인 테스트
+
+// 6. 구글 로그인 테스트
+
+
+// test("signup", async () =>{
+//   const signup = await users.signUpController();
+// })
+
+
+//   test("테스트 설명", () => {
+//   expect("검증 대상").toXxx("기대 결과");
+// });
+
