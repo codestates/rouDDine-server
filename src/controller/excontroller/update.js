9ac30fe79486c7c186f16c1a12ddf960773c4f73
leegeonwoo <kwoo.chris@gmail.com>
2022-09-11 10:57:47 +0900

    #3 운동카드 업데이트 개선 필요

--- src/controller/excontroller/update.js
+++ src/controller/excontroller/update.js
@@ -4,38 +4,42 @@ const { exercise } = require("../../orm/sequelize/models");
 module.exports = {
   //운동카드 수정하기 - patch방식
   update_exercise: async (req, res) => {
-    if (!req.body.workoutid) {
-      res.status(409).send({
-        message: "invalid request",
-      });
-    } else {
-      const ex_card = await exercise.findOne({
-        where: { id: req.body.workoutid },
-      });
-      if (ex_card) {
-        if (ex_card.default === true) {
-          res.status(200).send({
-            message: "기본 운동은 수정 불가입니다.",
-            result: ex_card,
-          });
-        } else {
+    const { workoutId } = req.body
+
+    try{
+      if(!workoutId){
+        return res.status(409).send({ "meesage" : "invalide request"})
+      }
+      else {
+        try{
+          const ex_card = await exercise.findOne({ 
+            where : { id : workoutId}
+          })
+          if(ex_card) {
+            if(ex_card.default) {
+              return res.status(400).send({ "message" : "기본 운동은 수정 불가입니다.",
+              "result" : ex_card })
+            }
+          }
+        }catch {
+          const {name,set_time,set_number,rest_time} = req.body;
           await ex_card.update({
-            name: req.body.name,
-            set_time: req.body.set_time,
-            set_number: req.body.set_number,
-            rest_time: req.body.rest_time,
-            memo: req.body.memo,
+            name : name,
+            set_time : set_time,
+            set_number: set_number,
+            rest_time : rest_time,
+            memo : memo
           });
-          res.status(200).send({
-            message: "update exersice card",
-            result: ex_card,
-          });
+          return res.status(200).send({
+            "message" : "update exersice card",
+            "result" : ex_card
+          })
         }
-      } else {
-        res.status(409).send({
-          message: "update fail",
-        });
+      }
+    }catch(err){
+      switch(err.status) {
+        case 409 :
+          return res.status(409).send({ "message" :"update fail"})
       }
     }
-  },
 };
